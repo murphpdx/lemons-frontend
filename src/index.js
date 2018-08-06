aee780d27dfb7d919b1613975d2f0cc8cdd2326a
Amanda Murphy <murphdizle@hotmail.com>
2018-08-06 14:11:10 -0700

    linter stuff

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import Home from './components/Home'
 import Settings from './components/Settings'
 
 const wrappedComponent = (
-        <Router basename="/">
-            <div>
-                <Route path="/" exact component={Home} />
-                <Route path="/settings" component={Settings} />
-            </div>
-        </Router>
+  <Router basename='/'>
+    <div>
+      <Route path='/' exact component={Home} />
+      <Route path='/settings' component={Settings} />
+    </div>
+  </Router>
 )
 
 injectTapEventPlugin()
 const render = () => ReactDOM.render(
-    wrappedComponent,
-    document.getElementById('app'))
+  wrappedComponent,
+  document.getElementById('app'))
 render()
