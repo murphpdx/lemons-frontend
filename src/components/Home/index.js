695380ff4deaaa59835869ddef556b0d21ad3d43
Amanda Murphy <amanda.murphy@cdk.com>
2018-08-07 15:28:44 -0700

    added search button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
 import { Search } from '../Search/Search'
-import { searchByYearAndMake } from './../../services/search';
+import { searchByYearAndMake } from './../../services/search'
 export class Home extends Component {
   constructor (props) {
     super(props)
-      console.log('---- DEBUG ----');
-      searchByYearAndMake('2000', 'saturn').then(response => {
-          console.log(response);
-          console.log('---------------');
-      })
+    console.log('---- DEBUG ----')
+    searchByYearAndMake('2000', 'saturn').then(response => {
+      console.log(response)
+      console.log('---------------')
+    })
   }
 
   render () {
-
-      return (
+    return (
       <div>
-        <div className="search-bar">
-            <Search />
+        <div className='search-bar'>
+          <Search />
         </div>
       </div>
     )
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
-import InputAdornment from '@material-ui/core/InputAdornment';
-import SearchOutlined from '@material-ui/icons/SearchOutlined';
+import InputAdornment from '@material-ui/core/InputAdornment'
+import SearchOutlined from '@material-ui/icons/SearchOutlined'
+import Button from '@material-ui/core/Button'
 
 export class Search extends Component {
   constructor (props) {
@@ -10,23 +11,24 @@ export class Search extends Component {
 
   render () {
     return (
-      <div>
+      <form>
         <div>
-            <TextField
-                id="search-input"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <SearchOutlined />
-                        </InputAdornment>
-                    ),
-                }}
-            />
+          <TextField
+            id='search-input'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <SearchOutlined />
+                </InputAdornment>
+              )
+            }}
+          />
+          <Button>
+                Search
+          </Button>
         </div>
-        <div>
-
-        </div>
-      </div>
+        <div />
+      </form>
     )
   }
 }
