1a95efcfa7e910141dc8f445ea1d74918965be3f
Amanda Murphy <murphdizle@hotmail.com>
2018-08-07 12:32:49 -0700

    date picker

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import DatePicker from 'react-datepicker'
+let moment = require('moment')
 
 export class Search extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      year: moment(),
+      make: '',
+      model: ''
+    }
+  }
+
+  handleDateChange (year) {
+    this.setState({
+      year: year
+    })
   }
 
   render () {
     return (
-      <div>
+      <form>
         <div>
-          <TextField />
+          <DatePicker
+            selected={this.state.year}
+            dateFormat='YYYY'
+            onChange={this.handleDateChange}
+          />
+          <TextField value={this.state.model} />
+          <TextField value={this.state.make} />
         </div>
         <div>
           <Button variant='outlined' color='primary' className='search-button'>
                   Search
           </Button>
         </div>
-      </div>
+      </form>
     )
   }
 }
