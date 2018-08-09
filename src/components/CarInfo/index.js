82d6e36703496c1a61a17eeab4f38188563dd511
justinshuck <justinshuck396@msn.com>
2018-08-09 15:04:11 -0700

    Cleaning up car info view

--- src/components/CarInfo/index.js
+++ src/components/CarInfo/index.js
@@ -3,76 +3,102 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import { Link } from 'react-router-dom'
-import Moment from 'react-moment'
+import {Link} from 'react-router-dom'
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Warning from '@material-ui/icons/Warning';
+import Collapse from '@material-ui/core/Collapse';
+import moment from 'moment';
 
 export class CarInfo extends React.Component {
-  constructor (props) {
-    super(props)
-    let carName = 'type of car' // placeholder
-    this.state = {
-      carName: carName
+    constructor (props) {
+        super(props)
+        this.state = {
+            expanded: false
+        }
+
+        this.handleExpandClick = this.handleExpandClick.bind(this);
     }
-  }
 
-  toLowerCase (string) {
-    var a = string
-    var b = a.toLowerCase()
-    return b
-  }
+    handleExpandClick = () => {
+        this.setState(state => ({ expanded: !state.expanded }));
+    };
 
-  render () {
-    const { carDetails } = this.props.location.state
-    return (
-      <div>
-        <Card>
-          <CardContent>
-            <Typography gutterBottom variant='display1'>
-              Car Information: {carDetails.Results[0].ModelYear} {carDetails.Results[0].Make} {carDetails.Results[0].Model}
-            </Typography>
+    render () {
+        const { carDetails } = this.props.location.state
+
+        return (
             <div>
-              <Button
-                size='large'
-                color='primary'
-                variant='outlined'
-                component={Link}
-                to={'../'}
-              >
-                back to search
-              </Button>
-            </div>
-            <br />
-            {carDetails.Results.map(recalls =>
-              <div key={recalls.NHTSACampaignNumber}>
                 <Card>
-                  <Typography gutterBottom variant='body2' color='textSecondary' />
-                  <Typography style={{marginBottom: 8, marginLeft: 8, marginRight: 6, marginTop: 8}}>
-                    <Typography style={{fontWeight: 'bold'}}>ReportReceivedDate:</Typography>
-                    <Moment format='MMMM Do, YYYY' unix>
-                      {((recalls.ReportReceivedDate.replace(/[^\d.]/g, '')).slice(0, 10))}
-                    </Moment>
-                    <br />
-                    <Typography style={{fontWeight: 'bold'}}>Component:</Typography> {this.toLowerCase(recalls.Component.toString())}
-                    <br />
-                    <Typography style={{fontWeight: 'bold'}}>Consequence:</Typography> {this.toLowerCase(recalls.Conequence.toString())}
-                    <br />
-                    <Typography style={{fontWeight: 'bold'}}>Remedy:</Typography> {this.toLowerCase(recalls.Remedy.toString())}
-                    <br />
-                    <Typography style={{fontWeight: 'bold'}}>Notes:</Typography> {this.toLowerCase(recalls.Notes.toString())}
-                    <br />
-                    <Typography style={{fontWeight: 'bold'}}>Summary:</Typography> {this.toLowerCase(recalls.Summary.toString())}
-                  </Typography>
+                    <CardContent>
+                        <Typography gutterBottom variant='display1'>
+                            Car
+                            Information: {carDetails.Results[ 0 ].ModelYear} {carDetails.Results[ 0 ].Make} {carDetails.Results[ 0 ].Model}
+                        </Typography>
+                        <div>
+                            <Button
+                                size='large'
+                                color='primary'
+                                variant='outlined'
+                                component={Link}
+                                to={'../'}
+                            >
+                                back to search
+                            </Button>
+                        </div>
+                        <br />
+                        {carDetails.Results.map(recalls =>
+                            <Card>
+                                <CardHeader
+                                    avatar={
+                                        <Avatar aria-label="Recall">
+                                            {recalls.Component.toString().substr(0, 3)}
+                                        </Avatar>
+                                    }
+                                    action={
+                                        <div>
+                                            <IconButton>
+                                                <Warning disabled='true' color="error"/>
+                                            </IconButton>
+                                            <IconButton
+                                                onClick={this.handleExpandClick}
+                                                aria-expanded={this.state.expanded}
+                                            >
+                                                <ExpandMoreIcon />
+                                            </IconButton>
+                                        </div>
+                                    }
+                                    title={recalls.Component.toString()}
+                                    subheader={moment(recalls.ReportReceivedDate).format('LLLL')}
+
+                                />
+                                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                                    <CardContent>
+                                        <Typography
+                                            style={{ fontWeight: 'bold' }}>Component
+                                            Tree:</Typography> {recalls.Component.toString()}
+                                        <br/> <br />
+                                        <Typography
+                                            style={{ fontWeight: 'bold' }}>Consequence:</Typography> {recalls.Conequence.toString()}
+                                        <br/> <br />
+                                        <Typography
+                                            style={{ fontWeight: 'bold' }}>Remedy:</Typography> {recalls.Remedy.toString()}
+                                        <br/> <br />
+                                        <Typography
+                                            style={{ fontWeight: 'bold' }}>Notes:</Typography> {recalls.Notes.toString()}
+                                        <br/> <br />
+                                        <Typography
+                                            style={{ fontWeight: 'bold' }}>Summary:</Typography> {recalls.Summary.toString()}
+                                    </CardContent>
+                                </Collapse>
+                            </Card>)}
+                    </CardContent>
                 </Card>
-                <Typography>
-                  <br />
-                </Typography>
-              </div>
-            )}
-          </CardContent>
-        </Card>
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 export default CarInfo
